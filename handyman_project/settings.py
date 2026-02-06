c3f1ae698bfa6894e80493c23a0e7aa4f5c128ae
Neo_codez <holyjazzy1@gmail.com>
2026-02-06 04:03:15 +0100

    Updated Allowed Hosts

--- handyman_project/settings.py
+++ handyman_project/settings.py
@@ -34,10 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1', 
-    'findus.onrender.com'
+ALLOWED_HOSTS = [ '*'
     
 ]
 
